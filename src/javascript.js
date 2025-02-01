1fce52e78c7f17d2c393b753fff83b7991f73be4
AdrianoMouraSilva <adr.moura21@hotmail.com>
2025-02-01 16:34:09 -0300

    Pagina com javascript

---
+++ src/javascript.js
@@ -0,0 +1,30 @@
+// variables
+
+// var -> Antigo
+// let -> Nova versões
+//const -> 
+var a = 10
+let b = 12
+const c = 10
+
+a = 'banana'
+
+console.log(a, b)
+
+//tipo de dados
+let name = 'Adriano'
+let age = 120
+let open = false 
+
+const person = {
+    name:'Adriano',
+    age: 15,
+    gender: 'male'
+}
+
+const persons = ['felipe','maria','joao']
+
+persons.push('Pablo');
+
+console.log(persons)
+
